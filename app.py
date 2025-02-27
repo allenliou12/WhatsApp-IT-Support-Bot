e5c16e0de7f229cebe8d551b03d16f9105795884
allenliou12 <116005322+allenliou12@users.noreply.github.com>
2025-02-27 08:28:21 +0800

    Removed unused packages

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from selenium.common.exceptions import NoSuchElementException
 from dotenv import load_dotenv
 import mysql.connector
 import time
-import pandas as pd
 from datetime import datetime
 import sys
 import random
